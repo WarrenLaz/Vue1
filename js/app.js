0db4f31484b904eb544ad6c459ba8a78d890df8f
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-02-09 00:42:47 -0500

    40%

--- js/app.js
+++ js/app.js
@@ -13,8 +13,19 @@ createApp({
     setup(){
         const data = reactive(Default_State)
 
+        const insertName = () => {
+            const user = data.inputName;
+            data.names.push(user);
+            data.inputName = '';
+        }
+
+        const removeName = (index) => {
+            data.names.splice(index,1)
+        }
         return{
-            data
+            data,
+            insertName,
+            removeName
         }
     }
 }).mount('#app')
