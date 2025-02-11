dfad1963df96e323d5fdad7c37c5195a67c4cb19
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-02-10 19:43:20 -0500

    100%

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 const {createApp, reactive, computed} = Vue;
 
-const Default_State = {
+var Default_State = {
     state: true,
     input:'',
     names:[],
-    error:'',
     showError: false,
     result:''
 }
@@ -13,10 +12,41 @@ createApp({
     setup(){
         const data = reactive(Default_State)
 
+        const isReady = computed(()=>{
+            return data.names.length > 1;
+        })
+        const randName = () =>{
+            return data.names[Math.floor(Math.random()*data.names.length)];
+        }
+        const showResults = () => {
+            data.state = false;
+            data.result = randName();
+        }
         const insertName = () => {
             const user = data.inputName;
+
+            valid_(user) ? 
+            data.showError = true : 
+            (()=>{
             data.names.push(user);
             data.inputName = '';
+            data.showError = false;
+            })();
+        }
+        const Start_Over = () =>{
+            data.state = true;
+            data.input = '';
+            data.names = [];
+            data.showError = false ;
+            data.result =  '';
+        }
+        const New_Name = () =>{
+            while((name = randName()) == data.result){
+            }
+            data.result = name;
+        }
+        const valid_ = (val) => {
+            return val==='' || data.names.includes(val);
         }
 
         const removeName = (index) => {
@@ -25,7 +55,11 @@ createApp({
         return{
             data,
             insertName,
-            removeName
+            removeName,
+            isReady,
+            showResults,
+            Start_Over,
+            New_Name
         }
     }
 }).mount('#app')
